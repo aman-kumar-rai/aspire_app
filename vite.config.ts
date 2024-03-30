7d0eeab734be4451e8a5aa666c3558c0e4d9619c
Aman Kumar <kumaraman140795@gmail.com>
2024-03-30 19:46:30 +0530

    Setup aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path"
+
+// @ts-ignore
+const root = resolve(__dirname, "./src")
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(root, "./"),
+      "@components": resolve(root, "./components")
+    }
+  }
 })
