bd16cd4eb59212bd41cea671ed9b83806de9d46f
Aman Kumar <kumaraman140795@gmail.com>
2024-03-30 20:23:31 +0530

    Setup vitest and react-testing-library

---
+++ src/components/App/App.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import App from "@components/App/App";
+
+
+it("can run basic js test", () => {
+    expect(1).toEqual(1);
+})
+
+
+it("renders App component", () => {
+    render(<App />);
+    screen.findByRole("heading");
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest"/>
+/// <reference types="vite/client"/>
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from "path"
@@ -8,6 +11,12 @@ const root = resolve(__dirname, "./src")
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    css: true,
+    setupFiles: "./src/test/setup.ts"
+  },
   resolve: {
     alias: {
       "@": resolve(root, "./"),
