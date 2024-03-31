7a28a0ae7351c513d15ac94c2daac1db9c89d9ac
Aman Kumar <kumaraman140795@gmail.com>
2024-03-31 19:06:01 +0530

    Add utilities for generating transactions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -25,7 +25,7 @@ const generateCVV = (): string => {
     return faker.finance.creditCardCVV();
 }
 
-const generateBalance = (min = 1000, max = 5000, dec = 2): string => {
+const generateAmount = (min = 1000, max = 5000, dec = 2): string => {
     return faker.finance.amount({
         min,
         max,
@@ -40,18 +40,50 @@ const formatExpiryDate = (date: Date): string => {
     return `${month} / ${year.slice(2)}`;
 }
 
+const formatDate = (date: Date): string => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+
+    return `${day} ${month} ${year}`
+}
+
 // todo: add return type declaration
-const generateCard = (name?: string) => {
+const generateCard = (name?: string, transactionsCount = 3) => {
     return {
         name: name ? name : generateName(),
         currency: getCurrency(),
         cardNumber: generateCardNumber(),
         expiry: formatExpiryDate(generateDate()),
         cvv: generateCVV(),
-        balance: generateBalance(),
+        balance: generateAmount(),
         isFrozen: false,
-        transactions: []
+        transactions: generateTransactions(transactionsCount)
     }
+}
+
+const generatePastDate = (refDate: Date = new Date()): Date => {
+    return faker.date.past({
+        refDate
+    })
+}
+
+// todo: add return type declaration
+const generateTransaction = (min = 10, max = 500, dec = 2) => {
+    return {
+        amount: generateAmount(min, max, dec),
+        type: Math.random() > 0.5 ? "credit" : "debit",
+        date: formatDate(generatePastDate()),
+        merchant: faker.company.name()
+    }
+}
+
+const generateTransactions = (size = 3) => {
+    const transactions = [];
+    for (let i = 0; i < size; i++) {
+        transactions.push(generateTransaction())
+    }
+    return transactions;
 }
 
 export {
