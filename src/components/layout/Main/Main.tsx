10096f5046862dc1db424ae73a22b80d74052717
Aman Kumar <kumaraman140795@gmail.com>
2024-03-31 16:45:51 +0530

    Add route support for pages

--- src/components/layout/Main/Main.tsx
+++ src/components/layout/Main/Main.tsx
@@ -1,8 +1,36 @@
+import { Routes, Route } from "react-router-dom";
 import styles from "./style.module.css";
+import Home from "@/components/pages/Home/Home.tsx"
+import Cards from "@/components/pages/Cards/Cards";
+import Payments from "@/components/pages/Payments/Payments";
+import Credit from "@/components/pages/Credit/Credit";
+import Settings from "@/components/pages/Settings/Settings";
 
 const Main = () => {
     return (
         <main className={styles.container}>
+            <Routes>
+                <Route
+                    path="*"
+                    element={<Home />}
+                />
+                <Route
+                    path="/cards"
+                    element={<Cards />}
+                />
+                <Route
+                    path="/payments"
+                    element={<Payments />}
+                />
+                <Route
+                    path="/credit"
+                    element={<Credit />}
+                />
+                <Route
+                    path="/settings"
+                    element={<Settings />}
+                />
+            </Routes>
 
         </main>
     )
--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,93 @@
+import { NavLink } from "react-router-dom";
 import styles from "./style.module.css";
+import Logo from "@components/icons/Logo/Logo";
+import Home from "@components/icons/Home/Home";
+import Cards from "@components/icons/Cards/Cards";
+import Payments from "@components/icons/Payments/Payments";
+import Credit from "@components/icons/Credit/Credit";
+import Settings from "@components/icons/Settings/Settings";
+
+const navItems = [
+    {
+        id: "home",
+        label: "Home",
+    },
+    {
+        id: "cards",
+        label: "Cards"
+    },
+    {
+        id: "payments",
+        label: "Payments"
+    },
+    {
+        id: "credit",
+        label: "Credit"
+    },
+    {
+        id: "settings",
+        label: "Settings"
+    },
+]
+
+
+
+function getIcon(id, label, active) {
+    switch (id) {
+        case "/":
+        case "home":
+            return (
+                <Home label={label} className={active ? "active" : ""} />
+            );
+        case "cards":
+            return (
+                <Cards label={label} className={active ? "active" : ""} />
+            );
+        case "payments":
+            return (
+                <Payments label={label} className={active ? "active" : ""} />
+            );
+        case "credit":
+            return (
+                <Credit label={label} className={active ? "active" : ""} />
+            );
+        case "settings":
+            return (
+                <Settings label={label} className={active ? "active" : ""} />
+            );
+    }
+}
 
 const Sidebar = () => {
     return (
         <aside className={styles.container}>
-
+            <div className={styles.header}>
+                <Logo label="logo" />
+                <p className={styles.description}>Trusted way of banking for 3,000+ SMEs and startups in Singapore</p>
+            </div>
+            <nav id="sidebar" className={styles.navbar}>
+                {navItems.map(({ id, label }) => {
+                    return (
+                        <NavLink
+                            to={`/${id}`}
+                            className={({ isActive }) => {
+                                return (isActive ? `${styles.activeNav} ${styles.nav}` : styles.nav)
+                            }
+                            }
+                        >
+                            {({ isActive }) => (
+                                <>
+                                    {getIcon(id, label, isActive)}
+                                    <span>{label}</span>
+                                </>
+                            )}
+                        </NavLink>
+                    )
+                })}
+            </nav>
         </aside>
     )
 }
+
 
 export default Sidebar;
