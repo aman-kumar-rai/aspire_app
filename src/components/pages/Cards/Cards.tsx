18d4977eb2028ed42978c775362977fd36366c25
Aman Kumar <kumaraman140795@gmail.com>
2024-03-31 21:25:30 +0530

    Create left panel of main content of cards page

--- src/components/pages/Cards/Cards.tsx
+++ src/components/pages/Cards/Cards.tsx
@@ -1,44 +1,9 @@
-import spendLimit from "@assets/spendLimit.png";
-import freezeCard from "@assets/freezeCard.png";
-import gPay from "@assets/gPay.png";
-import replaceCard from "@assets/replaceCard.png";
-import cancelCard from "@assets/cancelCard.png";
-
-import Card from "@/components/Card/Card";
 import CardHeader from "@components/CardHeader/CardHeader";
 import CardContent from "@components/CardContent/CardContent";
-import CardControls from "@/components/CardControls/CardControls";
 
 
 import styles from "./style.module.css";
 
-const cardControllers = [
-    {
-        id: "freezeCard",
-        label: "Freeze Card",
-        icon: freezeCard
-    },
-    {
-        id: "spendLimit",
-        label: "Set Spend Limit",
-        icon: spendLimit
-    },
-    {
-        id: "gPay",
-        label: "Add to GPay",
-        icon: gPay
-    },
-    {
-        id: "replaceCard",
-        label: "Replace Card",
-        icon: replaceCard
-    },
-    {
-        id: "cancelCard",
-        label: "Cancel Card",
-        icon: cancelCard
-    },
-]
 
 const Cards = () => {
 
@@ -48,7 +13,7 @@ const Cards = () => {
     }
 
     //* to open modal for new card
-    const onAddNewCard = () =>{
+    const onAddNewCard = () => {
         console.log("onAddNewCard")
     }
 
@@ -61,27 +26,9 @@ const Cards = () => {
                 onAddNewCard={onAddNewCard}
             />
             <CardContent
+                handleCard={handleCard}
             />
-            {/* <div className={styles.header}>
-                <p>
-                    Available balance
-                </p>
-                <button> Add new Button</button>
-            </div>
-            <div className={styles.tabSection}>
-                <p>My Card</p>
-                <p>All Company Card</p>
-            </div> */}
-            <div className={styles.main}>
-                <div className={styles.leftPanel}>
-                    {/* <Card /> */}
-                    <CardControls controllers={cardControllers} handleCard={handleCard} />
-                </div>
-                <div className={styles.rightPanel}>
-                    Hello
-                </div>
-            </div>
-
+                    
         </div>
     )
 }
