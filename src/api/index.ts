2babd8c5a7a1b4a6ce3cc4e1c27cd884b58dfde7
Aman Kumar <kumaraman140795@gmail.com>
2024-03-31 19:14:04 +0530

    Add fake API for adding card

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import { generateCard } from "@/utils"
+
+
+const addCard = (name: string) => {
+    return new Promise((resolve): void => {
+        setTimeout(() => {
+            resolve(generateCard(name))
+        }, 100 + Math.floor(Math.random() * 500))
+    })
+}
+
+export {
+    addCard
+}
